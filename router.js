11d5424e04452ac66538651abe0eec22ac817ea0
efrengarzon <95939840+efrengarzon@users.noreply.github.com>
2021-12-15 09:40:47 -0500

    Cambios de 15/12/2021

--- router.js
+++ router.js
@@ -11,14 +11,39 @@ router.get('/',(req, res)=>{
         }else{
            res.render('index',{results:results});
         }
-    })  
+    })   
 })
+
 //RUTA PARA CREAR REGISTROS
 router.get('/create', (req,res)=>{
     res.render('create');
 })
 
-const crud = require('./controlers/crud');
+//RUTA PARA EDITAR REGISTROS
+router.get('/edit/:id', (req, res)=>{
+    const id = req.params.id;
+    conexion.query('SELECT * FROM users WHERE id= ?',[id], (error,results)=>{
+        if(error){
+            throw error;
+        }else{
+            res.render('edit',{user:results[0]});
+        }
+    })
+});
+
+//RUTA PARA ELIMINAR REGISTROS
+router.get('/delete/:id', (req, res)=>{
+    const id = req.params.id;
+    conexion.query('DELETE FROM users WHERE id = ?', [id], (error, results)=>{
+        if(error){
+            throw error;
+        }else{
+            res.redirect('/');
+        }
+    })
+})
+const crud = require('./controllers/crud');
 router.post('/save', crud.save);
+router.post('/update', crud.update);
 
 module.exports = router;
